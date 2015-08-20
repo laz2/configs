0be68f7a1fc3e9aa53ab31869c7df3436b5c8bd9
Dmitry Lazurkin <dmitry.lazurki@synesis.ru>
2015-08-20 15:40:30 +0300

    ubuntu home

--- ubuntu/home/make_links.sh
+++ ubuntu/home/make_links.sh
@@ -7,3 +7,5 @@ ln -f -s $SCRIPT_ROOT_PATH/.profile ~/.profile
 ln -f -s $SCRIPT_ROOT_PATH/.bashrc ~/.bashrc
 ln -f -s $SCRIPT_ROOT_PATH/.bash_aliases ~/.bash_aliases
 ln -f -s $SCRIPT_ROOT_PATH/.bash_logout ~/.bash_logout
+ln -f -s $SCRIPT_ROOT_PATH/.tmux.conf ~/.tmux.conf
+ln -f -s $SCRIPT_ROOT_PATH/.gitconfig ~/.gitconfig
