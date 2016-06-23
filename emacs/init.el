095082fa35463052c12ce6ec51441c944b2542c0
Dmitry Lazurkin <dilaz03@gmail.com>
2016-06-23 12:18:33 +0300

    emacs, install

--- emacs/init.el
+++ emacs/init.el
@@ -116,6 +116,38 @@
 (line-number-mode t)
 (column-number-mode t)
 
+;; If emacs is run in a terminal, the clipboard- functions have no
+;; effect. Instead, we use of xsel, see
+;; http://www.vergenet.net/~conrad/software/xsel/ -- "a command-line
+;; program for getting and setting the contents of the X selection"
+(unless window-system
+  (when (getenv "DISPLAY")
+    ;; Callback for when user cuts
+    (defun xsel-cut-function (text &optional push)
+      ;; Insert text to temp-buffer, and "send" content to xsel stdin
+      (with-temp-buffer
+        (insert text)
+        ;; I prefer using the "clipboard" selection (the one the
+        ;; typically is used by c-c/c-v) before the primary selection
+        ;; (that uses mouse-select/middle-button-click)
+        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    ;; Call back for when user pastes
+    (defun xsel-paste-function()
+      ;; Find out what is current selection by xsel. If it is different
+      ;; from the top of the kill-ring (car kill-ring), then return
+      ;; it. Else, nil is returned, so whatever is in the top of the
+      ;; kill-ring will be used.
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (unless (string= (car kill-ring) xsel-output)
+          xsel-output )))
+    ;; Attach callbacks to hooks
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    ;; Idea from
+    ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
+    ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
+    ))
+
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (when (fboundp 'tool-bar-mode)
@@ -355,7 +387,7 @@
       (set-buffer-modified-p nil)))
 
   (defadvice dired-readin
-    (after dired-after-updating-hook first () activate)
+      (after dired-after-updating-hook first () activate)
     "Sort dired listings with directories first before adding marks."
     (my/dired-sort)))
 
@@ -496,7 +528,7 @@
          ("C-^" . crux-top-join-line))
   :config
   (defadvice crux-reopen-as-root
-    (after my/crux-reopen-as-root first () activate)
+      (after my/crux-reopen-as-root first () activate)
     (projectile-mode -1)))
 
 (use-package flycheck
@@ -561,7 +593,11 @@
 
 (use-package dockerfile-mode
   :ensure
-  :commands dockerfile-mode)
+  :commands dockerfile-mode
+  :init
+  (add-hook 'dockerfile-mode-hook
+            (lambda ()
+              (setq-local indent-tabs-mode nil))))
 
 (use-package markdown-mode
   :ensure
@@ -677,7 +713,7 @@
                              (js2-imenu-extras-mode t)))
 
   (defadvice js2-record-name-node
-    (after my/js2-record-name-node first () activate)
+      (after my/js2-record-name-node first () activate)
     (let ((node (ad-get-arg 0))
           (leftpos (js2-node-abs-pos node)))
       (js2-set-face leftpos
