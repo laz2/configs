76bc6564ae84e83e30d0474916aeafb481ed40e7
Dmitry Lazurkin <dilaz03@gmail.com>
2016-12-06 22:11:49 +0300

    emacs, ubuntu

--- emacs/init.el
+++ emacs/init.el
@@ -260,8 +260,8 @@
 
 (setq scroll-step 1)
 (setq scroll-preserve-screen-position 'always)
-(setq scroll-conservatively 10000)
-(setq auto-window-vscroll nil)
+;; (setq scroll-conservatively 10000)
+(setq auto-window-vscroll t)
 (setq default-tab-width 4)
 (global-hl-line-mode -1)
 (windmove-default-keybindings 'meta)
