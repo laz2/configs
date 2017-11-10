09c0d1f1098f367f51e3576266c22e6f0d20b355
Dmitry Lazurkin <dilaz03@gmail.com>
2017-11-10 18:41:54 +0300

    emacs

--- emacs/kstation-project.el
+++ emacs/kstation-project.el
@@ -2,43 +2,56 @@
 (setq flycheck-python-pycompile-executable "~/.virtualenvs/oldfashioned/bin/python3")
 (setq flycheck-python-flake8-executable "~/.virtualenvs/oldfashioned/bin/flake8")
 
+(defun kstation/django-test (arg file)
+  (compile
+   (format (concat
+            "cd %s && "
+            "source ~/.virtualenvs/oldfashioned/bin/activate && "
+            "source lxc/os/env/development.sh && "
+            "cd backend/s7 && "
+            "python -W ignore::DeprecationWarning:RemovedInDjango19Warning ./manage.py test %s--noinput %s")
+           (projectile-project-root)
+           (if arg "" "--keepdb ")
+           file)
+   t))
+
+(defun kstation/django-test-buffer-path ()
+  (s-replace "/" "."
+             (s-chop-suffix ".py"
+                            (file-relative-name
+                             (buffer-file-name)
+                             (expand-file-name
+                              (concat (projectile-project-root) "/backend/s7"))))))
+
 (defun kstation/django-test-app (arg)
   (interactive "P")
-  (compile (format (concat
-                    "cd %s && "
-                    "source ~/.virtualenvs/oldfashioned/bin/activate && "
-                    "source lxc/os/env/development.sh && "
-                    "cd backend/s7 && "
-                    "python -W ignore::DeprecationWarning:RemovedInDjango19Warning ./manage.py test %s--noinput")
-                   (projectile-project-root)
-                   (if arg "" "--keepdb ")) t))
+  (kstation/django-test arg ""))
 
 (defun kstation/django-test-file (arg)
   (interactive "P")
-  (compile (format (concat
-                    "cd %s && "
-                    "source ~/.virtualenvs/oldfashioned/bin/activate && "
-                    "source lxc/os/env/development.sh && "
-                    "cd backend/s7 && "
-                    "python -W ignore::DeprecationWarning:RemovedInDjango19Warning ./manage.py test --failfast %s--noinput %s")
-                   (projectile-project-root)
-                   (if arg "" "--keepdb ")
-                   (s-replace "/" "."
-                              (s-chop-suffix ".py"
-                                             (file-relative-name
-                                              (buffer-file-name)
-                                              (expand-file-name
-                                               (concat (projectile-project-root) "/backend/s7")))))) t))
+  (kstation/django-test arg (kstation/django-test-buffer-path)))
 
-(defun kstation/ui-compile-command ()
-  (interactive)
-  (compile (format (concat "cd %s && "
-                           "cd ui && "
-                           "grunt unit:dev --spec=%s --no-color")
-                   (projectile-project-root)
-                   (file-relative-name
-                    (buffer-file-name)
-                    (concat (projectile-project-root) "ui")) t)))
+(defun kstation/django-test-test (arg)
+  (interactive "P")
+  (let* ((imenu (save-excursion
+                  (python-imenu--build-tree)))
+         (defun-point (save-mark-and-excursion
+                        (beginning-of-defun)
+                        (point)))
+         (entries (-filter
+                   (lambda (el)
+                     (and (listp el)
+                          (string-match "^test_.+ (\\(async \\)?def)$" (car el))
+                          (= defun-point (cdr el))))
+                   imenu))
+         (entry (car entries)))
+    (when entry
+      (kstation/django-test arg
+                            (format
+                             "%s.%s.%s"
+                             (kstation/django-test-buffer-path)
+                             (first (split-string (car imenu)))
+                             (first (split-string (car entry))))))))
 
 (defun kstation/lint (arg)
   (interactive "P")
@@ -59,6 +72,7 @@
 
 (global-set-key (kbd "C-c C-t a") 'kstation/django-test-app)
 (global-set-key (kbd "C-c C-t r") 'kstation/django-test-file)
+(global-set-key (kbd "C-c C-t t") 'kstation/django-test-test)
 
 (dir-locals-set-class-variables
  'kstation-project
